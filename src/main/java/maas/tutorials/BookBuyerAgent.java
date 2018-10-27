1a76156ca91d0a3e453bbc5cad9c0f7ddcd1adc8
Sushant Vijay Chavan <gitecsvc@gmail.com>
2018-10-27 11:42:10 +0200

    Add method to discover seller agents
    Also added a gitIgnore

--- src/main/java/maas/tutorials/BookBuyerAgent.java
+++ src/main/java/maas/tutorials/BookBuyerAgent.java
@@ -7,29 +7,40 @@ import jade.content.lang.Codec;
 import jade.content.lang.sl.SLCodec;
 import jade.content.onto.basic.Action;
 import jade.core.Agent;
+import jade.core.AID;
 import jade.core.behaviours.*;
 import jade.domain.FIPANames;
 import jade.domain.JADEAgentManagement.JADEManagementOntology;
 import jade.domain.JADEAgentManagement.ShutdownPlatform;
 import jade.lang.acl.ACLMessage;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.MessageTemplate;
 
 
 @SuppressWarnings("serial")
 public class BookBuyerAgent extends Agent {
 	protected List<String> _titlesToPurchase;
 	protected List<String> _purchasedTitles;
-	
-	protected void setup() {
-	// Printout a welcome message
-		System.out.println("Hello! Buyer-agent "+getAID().getName()+" is ready.");
-		
-		_purchasedTitles = new Vector<>();
-		
-		determineTitlesToBuy();
-//		displayTitlesToBuy();
+
+    // The list of available seller agents
+    private AID[] sellerAgents;
+    
+    protected void setup() {
+    // Printout a welcome message
+        System.out.println("Hello! Buyer-agent "+getAID().getName()+" is ready.");
+        
+        _purchasedTitles = new Vector<>();
+        
+        determineTitlesToBuy();
+//        displayTitlesToBuy();
+        
+        discoverSellers();
 
         try {
- 			Thread.sleep(3000);
+            Thread.sleep(3000);
  		} catch (InterruptedException e) {
  			//e.printStackTrace();
  		}
@@ -73,10 +84,28 @@ public class BookBuyerAgent extends Agent {
 			System.out.println(_purchasedTitles);
 			System.out.println("==========================================\n");
 		}
-	}
+    }
+    
+    protected void discoverSellers() {
+     // Update the list of seller agents
+        DFAgentDescription template = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("book-seller");
+        template.addServices(sd);
+        try {
+            DFAgentDescription[] result = DFService.search(this, template);
+            sellerAgents = new AID[result.length];
+            for (int i = 0; i < result.length; ++i) {
+                sellerAgents[i] = result[i].getName();
+            }
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+    }
 
     // Taken from http://www.rickyvanrijn.nl/2017/08/29/how-to-shutdown-jade-agent-platform-programmatically/
-	private class shutdown extends OneShotBehaviour{
+    private class shutdown extends OneShotBehaviour{
 		public void action() {
 			ACLMessage shutdownMessage = new ACLMessage(ACLMessage.REQUEST);
 			Codec codec = new SLCodec();
